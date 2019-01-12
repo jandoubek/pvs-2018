4a73bed3cbfc12e3149cfd319100751891974266
Martin <martin.zemko@fjfi.cvut.cz>
2019-01-12 21:14:33 +0100

    Implementované webové užívateľské rozhranie

--- flask_server/main.py
+++ flask_server/main.py
@@ -1,13 +1,6 @@
-from flask import render_template
-import connexion
+from flask import Flask, render_template
+app = Flask(__name__)
 
-# Create the application instance
-app = connexion.App(__name__, specification_dir='./')
-
-# Read the swagger.yml file to configure the endpoints
-app.add_api('coffea.yml')
-
-# Create a URL route in our application for "/"
 @app.route("/")
 def login():
     return render_template("login.html")
@@ -31,7 +24,3 @@ def exploration():
 @app.route("/moje-recenze")
 def reviewList():
     return render_template("list.html")
-
-# If we're running in stand alone mode, run the application
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
